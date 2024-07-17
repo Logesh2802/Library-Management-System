30070e32c86850a94fd9e7160d8d275d3609a3fb
Logesh2802 <114215464+Logesh2802@users.noreply.github.com>
2024-07-17 12:00:19 +0530

    admin section added

---
+++ admin/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container p-3 mb-2 bg-primary text-white">
+            <div class="row">
+                <div class="col-md-12">
+                  <h4> &copy;  Online Library Management System |<a href="https://github.com/logesh2802" target="_blank"  > Designed by : Logesh Mudaliyar</a> </h4>
+                </div>
+
+            </div>
+        </div>
+    </section>
